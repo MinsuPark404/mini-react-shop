f74d97976ce46eeccee699fd8ba47c0aa38b1e36
PMS <parkms6490@gmail.com>
2023-09-07 17:53:46 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import { createContext, useState } from "react";
 
 import HomeComponent from "./component/HomeComponent";
 import DetailComponent from "./component/DetailComponent";
 
-export const GlobalContext = createContext()
+export const GlobalContext = createContext();
 
 function App() {
+  const navigate = useNavigate();
 
   return (
     <div className="App">
@@ -18,16 +19,17 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">SHOES4</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/">홈</Link>
-            <Link to="/detail">상세페이지</Link>
+            <Nav.Link onClick={() => { navigate('/')}}>홈</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail')}}>상세페이지</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
         <Route path="/" element={<HomeComponent />} />
-        <Route path="/detail" element={<DetailComponent />}/>
-      </Routes>      
+        <Route path="/detail" element={<DetailComponent />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
     </div>
   );
 }
