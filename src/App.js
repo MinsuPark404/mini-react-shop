551d78e207eb71eafc032b8a3481284da6e14abb
Minsu Park <parkms6490@gmail.com>
2023-09-08 02:41:30 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { createContext, useState } from "react";
 
 import HomeComponent from "./component/HomeComponent";
 import DetailComponent from "./component/DetailComponent";
+import ErrorPage from "./pages/ErrorPage";
+
 
 export const GlobalContext = createContext();
 
@@ -28,10 +30,43 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/detail" element={<DetailComponent />} />
-        <Route path="*" element={<div>404</div>} />
+        
+        {/* 리액트 라우터 3 : URL 파라미터로 상세페이지 100개 만들기 */}
+
+
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<About />} />
+        </Route>
+        <Route path="/event" element={<Event></Event>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route> */}
+
+
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
 }
+const About = () => {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+const Event = () => {
+  return (
+    <div>
+      <h1>오늘의 이벤트</h1>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  // const onGoBack: React.MouseEventHandler<HTMLButtonElement> = () => {
+  //   history.back();
+  // };
+  return (
+    <div className="error">
+    <div className="error__emoji-container">
+      {/* <Emoji /> */}
+    </div>
+    <div className="error__message">
+      <h1 className="error__message-code">404</h1>
+      <p className="error__message-title">Sorry, Page not found</p>
+      <p>The page you requested could not be found</p>
+    </div>
+    <div className="error__button-container">
+      {/* <button onClick={onGoBack}>
+        Go back
+      </button> */}
+    </div>
+  </div>
+  )
+}
+
+export default ErrorPage
